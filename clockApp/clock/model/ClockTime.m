b87cb63f4cc8713eeac4651a200b0505a44801b4
broy denty <denty_sun@wochacha.com>
2014-08-06 17:42:40 +0800

    add core data

---
+++ clockApp/clock/model/ClockTime.m
@@ -0,0 +1,17 @@
+//
+//  ClockTime.m
+//  clock
+//
+//  Created by broy denty on 14-8-6.
+//  Copyright (c) 2014年 denty. All rights reserved.
+//
+
+#import "ClockTime.h"
+
+
+@implementation ClockTime
+
+@dynamic ringTime;
+@dynamic enable;
+
+@end
